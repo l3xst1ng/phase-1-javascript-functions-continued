f3a420f64e4ab1cab0b040484f73b47ab41b8119
l3xst1ng <howdy.martt@gmail.com>
2024-03-24 23:25:58 +0300

    Js functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+// Quiz 1
+
+function saturdayFun(defaultActivity = "roller-skate") {
+  // Activity planned for Saturday (optional argument)
+  return `This Saturday, I want to ${defaultActivity}!`;
+}
+
+// Quiz 2
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+// Quiz 3
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
